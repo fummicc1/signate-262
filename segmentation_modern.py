d511d7e4901190170edffa655edebbbc7b550d6f
fummicc1 <fumiya.tennis1@gmail.com>
2023-04-17 20:33:10 +0900

    Try segmentation

---
+++ segmentation_modern.py
@@ -0,0 +1,351 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[59]:
+
+
+import os
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import pathlib
+
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset as BaseDataset
+
+import segmentation_models_pytorch as smp
+from segmentation_models_pytorch import utils as smp_utils
+import albumentations as albu
+import torch.nn as nn
+
+
+# In[60]:
+
+
+os.chdir("/home/fummicc1/codes/signate/")
+
+
+# In[61]:
+
+
+TRAIN_DIR = pathlib.Path('./train')
+VAL_DIR = pathlib.Path('./val')
+TEST_DIR = pathlib.Path('./test')
+
+
+# In[62]:
+
+
+x_train_dir = TRAIN_DIR / "modern" / "images"
+y_train_dir = TRAIN_DIR / "modern" / "masks"
+
+x_valid_dir = VAL_DIR / "modern" / "images"
+y_valid_dir = VAL_DIR / "modern" / "masks"
+
+x_test_dir = TEST_DIR / "modern" / "images"
+y_test_dir = TEST_DIR / "modern" / "masks"
+
+
+# In[63]:
+
+
+def get_training_augmentation():
+    train_transform = [
+        albu.PadIfNeeded(min_height=1216, min_width=1216, always_apply=True, border_mode=0),
+        albu.RandomCrop(height=1216, width=1216, always_apply=True)
+    ]
+    return albu.Compose(train_transform)
+    
+def get_validation_augmentation():
+    test_transform = [
+        albu.PadIfNeeded(1216, 1216)
+    ]
+    return albu.Compose(test_transform)
+
+def to_tensor(x: np.ndarray, **kargs):
+    return torch.tensor(x.transpose(2, 0, 1), dtype=torch.float)
+
+def get_preprocessing(preprocessing_fn):
+    _transform = [
+        albu.Lambda(image=preprocessing_fn) if preprocessing_fn is not None else albu.Compose([]),
+        albu.Lambda(image=to_tensor, mask=to_tensor)
+    ]
+    return albu.Compose(_transform)
+
+
+# In[64]:
+
+
+# 可視化用の関数
+def visualize(**images):
+    """PLot images in one row."""
+    n = len(images)
+    plt.figure(figsize=(16, 5))
+    for i, (name, image) in enumerate(images.items()):
+        plt.subplot(1, n, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(' '.join(name.split('_')).title())
+        plt.imshow(image)
+    plt.show()
+
+
+# In[65]:
+
+
+# パラメータ
+ENCODER = 'se_resnext50_32x4d'
+ENCODER_WEIGHTS = 'imagenet'
+ACTIVATION = None 
+CLASS_MAP = {
+  0: "0_background",
+  1: "1_overall",
+  2: "2_handwritten",
+  3: "3_typography",
+  4: "4_illustration",
+  5: "5_stamp",
+  6: "6_headline",
+  7: "7_caption",
+  8: "8_textline",
+  9: "9_table",
+}
+CLASSES = CLASS_MAP.values()
+DEVICE = 'cuda'
+
+
+# In[66]:
+
+
+class MyDataset(Dataset):
+    
+    def __init__(
+        self,
+        images_dir,
+        masks_dir,
+        augmentation=None,
+        preprocessing=None
+    ) -> None:
+        self.ids = os.listdir(images_dir)
+        self.images = [os.path.join(images_dir, id) for id in self.ids]
+        self.masks = [os.path.join(masks_dir, id) for id in self.ids]
+        
+        # クラス名の文字列
+        self.class_values = CLASS_MAP.keys()
+        
+        self.augmentation = augmentation
+        self.preprocessing = preprocessing
+        
+    def __len__(self) -> int:
+        return len(self.ids)
+        
+    # 3. 学習用データ(image)と特徴(mask)を返す__getitem__メソッドを作成
+    def __getitem__(self, i):
+        
+        # データの読み込み
+        image = cv2.imread(self.images[i])
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        mask = cv2.imread(self.masks[i], 0) // 25 # 色を強調するために25倍されている為
+        
+        # 学習対象のクラス(例えば、'car')のみを抽出
+        masks = [(mask == v) for v in self.class_values]
+        mask = np.stack(masks, axis=-1).astype('uint')
+        # print("mask.shape", mask.shape)
+        
+        # augmentation関数の適用
+        if self.augmentation:
+            sample = self.augmentation(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+        
+        # 前処理関数の適用
+        if self.preprocessing:
+            sample = self.preprocessing(image=image, mask=mask)
+            image, mask = sample['image'], sample['mask']
+            
+        return image, mask
+
+
+# In[67]:
+
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+# In[68]:
+
+
+# SMPを用いて学習済みモデルを取得(アーキテクチャはFPN)
+model = smp.FPN(
+    encoder_name=ENCODER, 
+    encoder_weights=ENCODER_WEIGHTS, 
+    classes=len(CLASSES), 
+    activation=ACTIVATION,
+)
+model = model.to(device=DEVICE).type(torch.float)
+model = nn.DataParallel(model).to(device=DEVICE).type(torch.float)
+
+
+# In[69]:
+
+
+# 損失関数
+loss = smp_utils.losses.DiceLoss(activation="softmax2d")
+
+# 評価関数
+metrics = [
+    smp_utils.metrics.IoU(threshold=0.5)
+]
+
+# 最適化関数
+optimizer = torch.optim.Adam([ 
+    dict(params=model.parameters(), lr=0.0001),
+])
+
+
+# In[70]:
+
+
+from torch.utils.data import DataLoader
+
+
+# データセットのインスタンスを作成
+train_dataset = MyDataset(
+    x_train_dir, 
+    y_train_dir, 
+    augmentation=get_training_augmentation(), 
+    preprocessing=get_preprocessing(None),
+)
+
+# データローダーの作成
+train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=12)
+
+
+# In[71]:
+
+
+from torch.utils.data import DataLoader
+
+# データセットのインスタンスを作成
+val_dataset = MyDataset(
+    x_valid_dir, 
+    y_valid_dir, 
+    augmentation=get_training_augmentation(), 
+    preprocessing=get_preprocessing(None),
+)
+
+# データローダーの作成
+val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False, num_workers=12)
+
+
+# In[72]:
+
+
+train_epoch = smp_utils.train.TrainEpoch(
+    model=model,
+    loss=loss,
+    metrics=metrics,
+    optimizer=optimizer,
+    device=DEVICE,
+    verbose=True,
+)
+
+valid_epoch = smp_utils.train.ValidEpoch(
+    model=model,
+    loss=loss,
+    metrics=metrics,
+    device=DEVICE,
+    verbose=True
+)
+
+
+# In[73]:
+
+
+epochs = 40
+max_score = 0
+
+if True:
+    for epoch in range(1, epochs + 1):
+        print(f"\n Epoch: {epoch}")
+        train_logs = train_epoch.run(train_loader)
+        valid_logs = valid_epoch.run(val_loader)
+        # 評価関数の値が更新されたらモデルを保存
+        if max_score < valid_logs['iou_score']:
+            max_score = valid_logs['iou_score']
+            torch.save(model, './best_model.pth')
+            print('Model saved!')
+
+        # エポック25以降は学習率(learning rate)を下げる      
+        if epoch == 25:
+            optimizer.param_groups[0]['lr'] = 1e-5
+            print('Decrease decoder learning rate to 1e-5!')
+
+
+# ## Classify
+
+# In[74]:
+
+
+# 可視化用の画像を取得するデータセットを作成(Augmentationなし)
+test_dataset_vis = MyDataset(
+    x_valid_dir, y_valid_dir, 
+)
+
+# 可視化用の関数
+def visualize(**images):
+    """PLot images in one row."""
+    n = len(images)
+    plt.figure(figsize=(16, 5))
+    for i, (name, image) in enumerate(images.items()):
+        plt.subplot(1, n, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.title(' '.join(name.split('_')).title())
+        plt.imshow(image)
+    plt.show()
+
+
+# In[75]:
+
+
+# 1. 学習モデルの読み込み
+best_model = torch.load(pathlib.Path("best_model.pth").as_posix())
+
+# 2. 推論用のデータセット、データローダーの作成
+test_dataset = MyDataset(
+    x_valid_dir, 
+    y_valid_dir, 
+    augmentation=get_validation_augmentation(), 
+    preprocessing=get_preprocessing(None),
+)
+test_dataloader = DataLoader(test_dataset)
+
+n_data = 2 # 確認するデータの数
+for i in range(n_data):
+    n = np.random.choice(len(test_dataset))
+
+    # 3. 新規データの取得
+    image_vis = test_dataset_vis[n][0].astype('uint8')
+    image, gt_mask = test_dataset[n]
+    gt_mask = gt_mask.squeeze()
+    
+    # 3. 新規データの推論
+    x_tensor = image.to(DEVICE).unsqueeze(0)
+    pr_mask = best_model.module.predict(x_tensor)    
+    pr_mask.squeeze_(dim=0)
+    pr_mask, _ = torch.max(pr_mask, dim=0)
+    pr_mask = (pr_mask.squeeze().cpu().numpy().round())       
+        
+    gt_mask = torch.argmax(gt_mask, dim=0)
+
+    print(pr_mask.shape, gt_mask.shape)
+        
+    # 4. 可視化
+    visualize(
+        image=image_vis, 
+        ground_truth_mask=gt_mask, 
+        predicted_mask=pr_mask
+    )
+
